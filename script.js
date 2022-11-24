8e0809a4fef25ae0c19fb636b37824c4a09170ab
ManuelBravaccini <manuelbravaccini2000@gmail.com>
2022-11-24 18:07:19 +0100

    Added tie scrpit and console log of each number

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ const button = document.querySelector('button');
 button.addEventListener('click', function(){
     let diceUser = document.getElementById("diceUser").value;
     let dice = Math.floor(Math.random() * 6) + 1;
-
-    if (diceUser > dice){
-        document.querySelector("h1").innerHTML = "Hai vinto! Al PC è uscito il numero " + dice + ".";
+    
+    if (diceUser <= 6) {
+        console.log(diceUser);
+        console.log(dice);
+        if (diceUser > dice){
+            document.querySelector("h1").innerHTML = "Hai vinto! Al PC è uscito il numero " + dice + ".";
+        } else if (diceUser == dice) {
+            document.querySelector("h1").innerHTML = "Pareggio! Al PC è uscito il numero " + dice + ".";
+        } else {
+            document.querySelector("h1").innerHTML = "Hai perso! Al PC è uscito il numero " + dice + ".";
+        }
     } else {
-        document.querySelector("h1").innerHTML = "Hai perso! Al PC è uscito il numero " + dice + ".";
+        document.querySelector("h1").innerHTML = "Hai inserito un numero superiore a 6! Hai perso!";
     }
-    
-    
-    
-    /*console.log(dice)*/
-    /*document.getElementById('').innerHTML = ;*/
 })
